c17ffc8b3349722624a357dc21abad2c14edeb36
erichestella <erichestella05@gmail.com>
2024-01-23 00:52:24 +0800

    variables to define the numbers

--- check if the given are the same.py
+++ check if the given are the same.py
@@ -4,3 +4,6 @@ print('\nCHECK IF THE FIRST NUMBER ARE THE SAME.')
 # this function is for checking numbers if the first and last are the same
 def check(numbers):
     print('\nGIVEN: ', numbers)
+#these are variable to define the numbers
+    first_number = numbers[0]
+    last_number = numbers[-1]
