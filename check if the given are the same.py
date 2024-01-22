ebb16ea311d50d947b4793c049a01bfdfb2cbd36
erichestella <erichestella05@gmail.com>
2024-01-23 00:49:47 +0800

    it checks the numbers if the first and last are the same

--- check if the given are the same.py
+++ check if the given are the same.py
@@ -1,3 +1,6 @@
 #add some title 
 print('\nCHECK IF THE FIRST NUMBER ARE THE SAME.')
 
+# this function is for checking numbers if the first and last are the same
+def check(numbers):
+    print('\nGIVEN: ', numbers)
